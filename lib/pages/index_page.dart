2535fad15581773a974109a54c4ef7d30a359f26
Admin <821176301@qq.com>
2019-05-19 22:54:05 +0800

    底部导航栏切换成功

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,13 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'home_page.dart';
+import 'cart_page.dart';
+import 'category_page.dart';
+import 'member_page.dart';
 
-class IndexPage extends StatelessWidget {
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页')
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.search),
+        title: Text('分类')
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.shopping_cart),
+        title: Text('购物车')
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.profile_circled),
+        title: Text('会员中心')
+    )
+  ];
+
+  final List tabBodies = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage()
+  ];
+
+  int currentIndex = 0;
+
+  var currentPage ;
+
+  @override
+  void initState() {
+    super.initState();
+    currentPage = tabBodies[currentIndex];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('百姓生活+'),),
-      body: Center(
-        child: Text('百姓生活+'),
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        items: bottomTabs,
+        onTap: (index){
+          setState(() {
+            currentIndex = index;
+            currentPage = tabBodies[currentIndex];
+          });
+        },
       ),
+      body: currentPage,
     );
   }
 }
+
