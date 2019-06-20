49ce31ad0f77f4c4d6395ef848f9628924da82d6
Admin <821176301@qq.com>
2019-06-21 00:32:28 +0800

    我的页面头部完成

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -48,8 +49,14 @@ class SwiperCustom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750,height: 1334)..init(context);
+    print('设备像素密度：${ScreenUtil.pixelRatio}');
+    print('设备的高：${ScreenUtil.screenHeight}');
+    print('设备的宽：${ScreenUtil.screenWidth}');
+
     return Container(
-      height: 333,
+      height: ScreenUtil().setWidth(333),
+      width: ScreenUtil().setHeight(750),
       child: Swiper(
         itemBuilder: (BuildContext context, int index){
           return Image.network('${swiperDateList[index]['image']}',fit: BoxFit.fill,);
--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MemberPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Member Page'),
+      appBar: AppBar(
+        title: Text('会员中心'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _topHeader()
+        ],
+      )
+    );
+  }
+  
+  Widget _topHeader(){
+    return Container(
+      width: ScreenUtil().setWidth(750),
+      padding: EdgeInsets.all(20),
+      color: Colors.pinkAccent,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            child: ClipOval(
+              child: Image.network('http://blogimages.jspang.com/blogtouxiang1.jpg'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              '四级五次郎',
+              style: TextStyle(
+                fontSize: ScreenUtil().setSp(36),
+                color: Colors.black54
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
