1621592542cebf8ee3a0af25bd39f139fcff1676
Admin <821176301@qq.com>
2019-06-30 16:45:22 +0800

    详情页底部布局完成

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -5,6 +5,7 @@ import './details_page/details_top_area.dart';
 import './details_page/details_explain.dart';
 import './details_page/details_tabbar.dart';
 import './details_page/details_web.dart';
+import './details_page/detalils_bottom.dart';
 
 class DetailsPage extends StatelessWidget {
   final String goodsId;
@@ -27,15 +28,24 @@ class DetailsPage extends StatelessWidget {
         future: _getBackInfo(context),
         builder: (context,snapshot){
           if(snapshot.hasData){
-            return Container(
-              child: ListView(
-                children: <Widget>[
-                  DetailsTopArea(),
-                  DetailsExplain(),
-                  DetailsTabBar(),
-                  DetailsWeb()
-                ],
-              ),
+            return Stack(
+              children: <Widget>[
+                Container(
+                  child: ListView(
+                      children: <Widget>[
+                      DetailsTopArea(),
+                      DetailsExplain(),
+                      DetailsTabBar(),
+                      DetailsWeb()
+                      ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  child: DetailsBottom(),
+                )
+              ],
             );
           }else{
             return Text('加载中...');
